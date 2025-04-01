9ef11c71244a085433d3d16fd746505e8df76a23
unknown <genesisdanielamartinezcardenas@gmail.com>
2025-03-31 17:36:53 -0700

    hice modificaciones

--- autocomplite.js
+++ autocomplite.js
@@ -1,3 +1,28 @@
-const createAutoComplete =({})=>{
+const createAutoComplete =({root,
+rederOption,OnOptionSelect,InputValue,fetchData})=>{
+//Funcion autoComplete
 
+root.innerHtml=`
+<label><b>Busqueda</b></label>
+<input class=¨Input¨ />
+<div class="dropdown">
+<div class="dropdown-menu">
+<div class="dropdown-content results"></div>
+</div>
+</div>
+`
+}
+
+const input = root.querySelector('input')
+const dropdown = root.querySelector('.dropdown')
+const resultWrapper = root.querySelector('.result')
+
+const debonce =(func, delay = 1000) => {
+    let timeoutid
+    return(...args) => {
+        clearTimeout(timeoutid)
+        timeoutid = setTimeout(() => {
+            func.apply(null,args)
+        }, delay)
+    }
 }
