eaad1698d6169e3458516699e631555e52cc329d
unknown <genesisdanielamartinezcardenas@gmail.com>
2025-04-07 17:23:07 -0700

    hice nuevamente cambios

--- autocomplite.js
+++ autocomplite.js
@@ -1,29 +1,54 @@
-const createAutoComplete =({root,
-rederOption,OnOptionSelect,InputValue,fetchData})=>{
-//Funcion autoComplete
+const createAutoComplete = ({root, rederOption, noOptionSelect, inputValue, fetchData}) => {
+    //Funcion autocomplete
 
-root.innerHtml=`
-<label><b>Busqueda</b></label>
-<input class=¨Input¨ />
-<div class="dropdown">
-<div class="dropdown-menu">
-<div class="dropdown-content results"></div>
-</div>
-</div>
-`
+    root.innerHTML =`
+    <label><b>Busqueda</b></label>
+    <input class="input" />
+    <div class="dropdown">
+        <div class="dopdown-menu">
+            <div class="dropdown-content results"></div>
+        </div>
+    </div>
+    `
 }
 
 const input = root.querySelector('input')
 const dropdown = root.querySelector('.dropdown')
 const resultWrapper = root.querySelector('.result')
 
-const debonce =(func, delay = 1000) => {
-    let timeoutid
+const debonce = (func, delay = 1000) =>{
+    let timeoutId
     return(...args) => {
-        clearTimeout(timeoutid)
-        timeoutid = setTimeout(() => {
+        clearTimeout(timeoutId)
+        timeoutId = setTimeout(() => {
             func.apply(null,args)
         }, delay)
     }
+}
+
+const onInput = async event => {
+    const items = await fetchData(event.target,value)
+    console.log("Movie",items)
+}
+
+if (!items.length){
+    dropdown.classList.add('is-active')
+    return
+}
+
+resultWrapper.innerHTML = ''
+dropdown.classList.add('is-active')
+for(let item of items){
+    const option = document.createElement('a')
+    
+    option.classList.add('dropdown-item')
+    option.innerHTML = renderOption(item)
+    option.addEventListener('click',() => {
+        dropdown.classList.remove('is-active')
+        input.value = inputValue(item)
+        onOptionSelect(item)
+        console.log("onMovieSelect")
+    })
+    resultWrapper.appendChild(option)
 }
 
